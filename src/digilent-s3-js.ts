97afbe23db420584aa16db095501b3f5196def6c
Sam Kristoff <sam.kristoff@digilent.com>
2018-01-10 22:54:46 +0000

    Update module for generic use case, rename functions and update function parameters

--- src/digilent-s3-js.ts
+++ src/digilent-s3-js.ts
@@ -1,27 +1,45 @@
-//import { S3 } from 'aws-sdk';
 declare var AWS: any;
 
 export class DigilentS3Js {
 
-    public s3: any;
-    public bucketName: string = 'digilent-test';
-    public awsRootUrl: string = 'https://s3-us-west-2.amazonaws.com/';
+    private s3: any;
+    private bucketName: string = '';
+    private awsRootUrl: string = '';
 
     constructor(
+        region: string = 'us-west-2',
+        bucketName: string = ''
     ) {
+        this.initialize(region, bucketName);
+    }
+
+    /********************************************************************************
+     * Initialize the S3 object configuration options.
+     * @param region The AWS region.
+     * @param bucketName The name of the AWS S3 bucket to set as the default ('active') bucket.
+     ********************************************************************************/
+    public initialize(region: string = 'us-west-2', bucketName: string = '') {
+        this.awsRootUrl = 'https://s3-' + region + '.amazonaws.com/';
+        this.bucketName = bucketName;
+
+        //Create internal S3 Object
         this.s3 = new AWS.S3({
             apiVersion: '2006-03-01',
             params: { Bucket: this.bucketName }
         });
-        console.log('DigilentS3Js Constructor Complete');
     }
 
-    //List all objects in the specified folder
-    public listFolder(path: string): Promise<Array<string>> {
+    /********************************************************************************
+     * List all objects in the specified bucket with the specified prefix.  This function returns a promise that resolves with an array of string object paths.  The prefix is stripped from all object paths before returning.  If no bucketname is provided the active bucket is used.  If no prefix is provided all objects are returned.
+     * @param prefix An object prefix (path) filter to apply.  Only objects with the specified prefix will be returned.
+     * @param bucketName The name of the S3 bucket to list. Defaults to the 'active' bucket if not specified.
+     ********************************************************************************/
+    public listObjects(prefix: string = '', bucketName: string = ''): Promise<Array<string>> {
         return new Promise((resolve, reject) => {
+
             let params = {
-                Bucket: this.bucketName,
-                Prefix: path,
+                Bucket: bucketName == '' ? this.bucketName : bucketName,
+                Prefix: prefix,
             };
             this.s3.listObjects(params, (err, data) => {
                 if (err) {
@@ -29,12 +47,34 @@ export class DigilentS3Js {
                 } else {
                     let objectNames = [];
                     data.Contents.forEach((object) => {
-                        let name = object.Key.substring(path.length);
+                        let name = object.Key.substring(prefix.length);
                         if (name.length > 0) {
                             objectNames.push(name);
                         }
                     });
                     resolve(objectNames);
+                }
+            });
+        });
+    }
+
+    /********************************************************************************
+     * Retrieve an object from S3.  This function returns a promise that resolves with the binary object.
+     * @param key The full object name including the prefix/path.
+     * @param bucketName The name of the S3 bucket containing the object. Defaults to the 'active' bucket if not specified.
+     ********************************************************************************/
+    public getObject(objectName: string, bucketName: string = ''): Promise<string> {
+        return new Promise((resolve, reject) => {
+            let params = {
+                Bucket: bucketName == '' ? this.bucketName : bucketName,
+                ResponseContentEncoding: 'null',
+                Key: objectName
+            };
+            this.s3.getObject(params, function (err, data) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(data.Body);
                 }
             });
         });
